aaf4359faeca04a5da6f5f97328ec4cff50ec64c
Junxiao Shi <git@mail1.yoursunny.com>
2015-05-02 00:11:02 -0700

    topo: GridTopo.makeHostName

--- topo/GridTopo.py
+++ topo/GridTopo.py
@@ -2,34 +2,47 @@ from mininet.topo import Topo
 
 class GridTopo(Topo):
     "Grid topology."
+
+    MAX_NROWS = 256 # limited by IP assignment
+    MAX_NCOLS = 256
+
+    @staticmethod
+    def makeHostName(row, col):
+        return 'h%sx%s' % (row, col)
+
+    @staticmethod
+    def parseHostName(hostName):
+        row, col = [ int(x) for x in hostName[1:].split('x') ]
+        return (row, col)
+
     def build(self, nRows=2, nCols=2):
         self.nRows = nRows
         self.nCols = nCols
         for row in range(nRows):
             for col in range(nCols):
-                host = self.addHost('h%sx%s' % (row, col))
+                host = self.addHost(self.makeHostName(row, col))
                 if col > 0:
-                    self.addLink('h%sx%s' % (row, col - 1), host)
+                    self.addLink(self.makeHostName(row, col - 1), host)
                 if row > 0:
-                    self.addLink('h%sx%s' % (row - 1, col), host)
+                    self.addLink(self.makeHostName(row - 1, col), host)
 
     def getPortAndIp(self, hostName, side):
-        (row, col) = [ int(x) for x in hostName[1:].split('x') ]
+        (row, col) = self.parseHostName(hostName)
         otherHost = None
         n = None
         if side == 'L':
-            otherHost = 'h%sx%s' % (row, col - 1)
+            otherHost = self.makeHostName(row, col - 1)
             col = col - 1
             n = 2
         elif side == 'R':
-            otherHost = 'h%sx%s' % (row, col + 1)
+            otherHost = self.makeHostName(row, col + 1)
             n = 1
         elif side == 'U':
-            otherHost = 'h%sx%s' % (row - 1, col)
+            otherHost = self.makeHostName(row - 1, col)
             row = row - 1
             n = 6
         elif side == 'D':
-            otherHost = 'h%sx%s' % (row + 1, col)
+            otherHost = self.makeHostName(row + 1, col)
             n = 5
 
         if otherHost is None:
@@ -49,7 +62,7 @@ class GridTopo(Topo):
            LEFT 10.row.col.2, RIGHT 10.row.col.1, UP 10.row.col.6, DOWN 10.row.col.5"""
         for row in range(self.nRows):
             for col in range(self.nCols):
-                hostName = 'h%sx%s' % (row, col)
+                hostName = self.makeHostName(row, col)
                 host = net.get(hostName)
 
                 for side in ['L', 'R', 'U', 'D']:
